ac31ed2577b5f5498c119c8fb93a13311b713f01
Edward Hughes <hughes5683@gmail.com>
2019-04-18 22:49:30 +0000

    Updates

---
+++ precache-manifest.2acad11c361346ecc3bf118d720b62d7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "67fe9042a37993dd0d7d",
+    "url": "/static/js/4.ccba6096.chunk.js"
+  },
+  {
+    "revision": "cb91b1fb6270a7c21352",
+    "url": "/static/css/main.db58a94c.chunk.css"
+  },
+  {
+    "revision": "2315c50f764cb9cbf065",
+    "url": "/static/js/runtime~main.46238d27.js"
+  },
+  {
+    "revision": "9d5316ce6667ebaa475d",
+    "url": "/static/js/2.732d3b4e.chunk.js"
+  },
+  {
+    "revision": "d1462159bd894600cd1df46b09efbc8e",
+    "url": "/static/media/candy-crush.d1462159.png"
+  },
+  {
+    "revision": "f1f1375b1b346b989563",
+    "url": "/static/js/3.0b6cf120.chunk.js"
+  },
+  {
+    "revision": "c3517ad786a71fd13198546f205a3c41",
+    "url": "/static/media/disorientis-banner-no-title.c3517ad7.png"
+  },
+  {
+    "revision": "cb91b1fb6270a7c21352",
+    "url": "/static/js/main.704dc0d1.chunk.js"
+  },
+  {
+    "revision": "241ede6fb3799fbd91e1",
+    "url": "/static/js/7.9e9fd928.chunk.js"
+  },
+  {
+    "revision": "ccdcd605a84dd61ac021",
+    "url": "/static/js/5.afe6a4f2.chunk.js"
+  },
+  {
+    "revision": "964bc119d047d0619d7b",
+    "url": "/static/js/6.1ccc1547.chunk.js"
+  },
+  {
+    "revision": "ccdcd605a84dd61ac021",
+    "url": "/static/css/5.8c3bf2ab.chunk.css"
+  },
+  {
+    "revision": "67fe9042a37993dd0d7d",
+    "url": "/static/css/4.f8108090.chunk.css"
+  },
+  {
+    "revision": "f1f1375b1b346b989563",
+    "url": "/static/css/3.a841bb90.chunk.css"
+  },
+  {
+    "revision": "2691e579de07f7fb03e2ae580bf2589b",
+    "url": "/index.html"
+  }
+];
