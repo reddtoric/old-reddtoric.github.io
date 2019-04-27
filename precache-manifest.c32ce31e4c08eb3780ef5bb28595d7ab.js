3007aaad7e6432ccbaa23657df34c6c20bba169c
Edward Hughes <hughes5683@gmail.com>
2019-04-27 02:33:06 +0000

    Updates

--- precache-manifest.c32ce31e4c08eb3780ef5bb28595d7ab.js
+++ precache-manifest.c32ce31e4c08eb3780ef5bb28595d7ab.js
@@ -1,35 +1,35 @@
 self.__precacheManifest = [
   {
-    "revision": "d124fef6c5a6fd8c83b3",
+    "revision": "483e9b97d7848aef1d22",
     "url": "/static/css/4.a841bb90.chunk.css"
   },
   {
-    "revision": "18047e6b8b066c325872",
+    "revision": "6574eaf4462ae5a2fa04",
     "url": "/static/css/main.db58a94c.chunk.css"
   },
   {
-    "revision": "4d01cf48824fc421a260",
-    "url": "/static/js/runtime~main.dcd8ad51.js"
+    "revision": "8a10d13e9cf745423588",
+    "url": "/static/js/runtime~main.ca934fca.js"
   },
   {
-    "revision": "40f1f90b35d1bfab566b",
-    "url": "/static/js/2.7422fa65.chunk.js"
+    "revision": "9d5316ce6667ebaa475d",
+    "url": "/static/js/2.732d3b4e.chunk.js"
   },
   {
     "revision": "d567bce4acc131f69651",
     "url": "/static/js/7.7af83d3e.chunk.js"
   },
   {
-    "revision": "c63f5d62ab67e63e135b",
-    "url": "/static/js/3.7e494cbd.chunk.js"
+    "revision": "594b5bb04af9dba42633",
+    "url": "/static/js/3.4f5364cd.chunk.js"
   },
   {
-    "revision": "18047e6b8b066c325872",
-    "url": "/static/js/main.29c7d323.chunk.js"
+    "revision": "6574eaf4462ae5a2fa04",
+    "url": "/static/js/main.5a20765b.chunk.js"
   },
   {
-    "revision": "d124fef6c5a6fd8c83b3",
-    "url": "/static/js/4.6969613e.chunk.js"
+    "revision": "483e9b97d7848aef1d22",
+    "url": "/static/js/4.4345c050.chunk.js"
   },
   {
     "revision": "39f7c43bcd84bdc8652e",
@@ -44,11 +44,11 @@ self.__precacheManifest = [
     "url": "/static/css/5.8c3bf2ab.chunk.css"
   },
   {
-    "revision": "c63f5d62ab67e63e135b",
+    "revision": "594b5bb04af9dba42633",
     "url": "/static/css/3.cef494ef.chunk.css"
   },
   {
-    "revision": "e27d106d86b63ac1cf4c12d25b2cdece",
+    "revision": "9a9cabf1fa31b594cc783e65720a255d",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8e7ca3c044d5e9d3c2a835065ae0d41.js"
+  "/precache-manifest.c32ce31e4c08eb3780ef5bb28595d7ab.js"
 );
 
 workbox.clientsClaim();
